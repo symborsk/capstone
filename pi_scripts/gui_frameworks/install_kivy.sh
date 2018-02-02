272588bc7c82d75048786e8f3d0f07f18b76587d
Joey-Michael Fallone <jm@sp0d3rm0b1le.local>
2018-02-02 14:11:09 -0700

    Added install script for kivy gui framework

---
+++ pi_scripts/gui_frameworks/install_kivy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Instally Kivy on Ubuntu MATE for PI"
+
+echo "Dependencies"
+sudo apt-get update
+sudo apt-get install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev \
+   pkg-config libgl1-mesa-dev libgles2-mesa-dev \
+   python-setuptools libgstreamer1.0-dev git-core \
+   gstreamer1.0-plugins-{bad,base,good,ugly} \
+   gstreamer1.0-alsa python-dev libmtdev-dev \
+   xclip
+
+echo "Install cpython"
+sudo pip3 install -U Cython==0.27.3
+
+echo "Install kiv from github"
+sudo pip3 install git+https://github.com/kivy/kivy.git@master
+
+echo "Script finished."
