5b8588f50307bc472ac051c7cf7559bbef4d0f65
Joey-Michael Fallone <jm@sp0d3rm0b1le.local>
2018-02-01 21:38:53 -0700

    I want this link

--- pi_image/gui/test.py
+++ pi_image/gui/test.py
@@ -3,6 +3,7 @@ from tkinter import *
 # http://python-textbok.readthedocs.io/en/1.0/Introduction_to_GUI_Programming.html
 # fullscreen: https://stackoverflow.com/questions/7966119/display-fullscreen-mode-on-tkinter
 # button orientation: https://stackoverflow.com/questions/31128780/tkinter-how-to-make-a-button-center-itself
+# wizard: https://stackoverflow.com/questions/41332955/creating-a-wizard-in-tkinter
 class WelcomeScreen:
     welcome_message = "Welcome to AI Sensor Hub setup! Let's guide you through a few steps to get your hub connected to the server."
     def __init__(self, master):
@@ -22,7 +23,7 @@ class WelcomeScreen:
         # self.back_button.place(relx=0.0, rely=0.0, anchor=SE)
 
         self.close_button = Button(master, text = "Close", command = master.quit)
-        
+
 
     def next(self):
         print("Call next data")
