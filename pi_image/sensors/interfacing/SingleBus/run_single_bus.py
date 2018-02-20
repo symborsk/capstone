78f3c4511906db3ddc7db39e18de5d0bc4cd1e61
Joey-Michael Fallone <jm@sp0d3rm0b1le.local>
2018-02-20 11:28:17 -0700

    Fixed python quirk

--- pi_image/sensors/interfacing/SingleBus/run_single_bus.py
+++ pi_image/sensors/interfacing/SingleBus/run_single_bus.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
 	# https://stackoverflow.com/questions/13045593/using-sudo-with-python-script
 	sudoPassword = 's3ns0r' # encrypt this lol
 
-	for sensor, pin in single_bus_sensors:
+	for sensor, pin in single_bus_sensors.items():
 		command = './SingleBus ' + str(pin) + ' > .data/' + sensor + '.dat'
 		p = os.system('echo %s|sudo -S %s' % (sudoPassword, command))
 
