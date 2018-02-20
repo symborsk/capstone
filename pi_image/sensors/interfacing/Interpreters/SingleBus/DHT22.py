3b5f62a9d9d8f3aa3d16e166c7a69e6572dff5e6
Joey-Michael Fallone <jm@sp0d3rm0b1le.local>
2018-02-20 11:36:59 -0700

    Various bug fixes

--- pi_image/sensors/interfacing/Interpreters/SingleBus/DHT22.py
+++ pi_image/sensors/interfacing/Interpreters/SingleBus/DHT22.py
@@ -1,4 +1,4 @@
-with open("../../SingleBus/.data/dht22_1") as file:
+with open("../../SingleBus/.data/dht22_1.dat") as file:
 	for line in file:
 		pass
 
@@ -6,8 +6,8 @@ with open("../../SingleBus/.data/dht22_1") as file:
 read_bytes = line.split(" ")
 
 # from adafruit library
-humidity = (read_bytes[0] * 256 + read_bytes[1]) / 10.0
-temp =     ((read_bytes[2] & 0x7F) * 256 + data[3]) / 10.0
+humidity = (int(read_bytes[0]) * 256 + int(read_bytes[1])) / 10.0
+temp =     ((int(read_bytes[2]) & 0x7F) * 256 + int(data[3])) / 10.0
 
 print("humidity: " + humidity)
 print("temp: " + temp)
