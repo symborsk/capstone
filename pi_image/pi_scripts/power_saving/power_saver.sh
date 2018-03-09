8b78681dea3aa6ea23f95b09e20b4c1939d4a94e
Joey-Michael Fallone <jm@sp0d3rm0b1le.local>
2018-03-09 14:38:09 -0700

    Added power-saving disabling of hardware

---
+++ pi_image/pi_scripts/power_saving/power_saver.sh
@@ -0,0 +1,9 @@
+# power saving tricks
+
+# requires reboot
+sudo mv /home/thor/capstone/pi_image/pi_scripts/power_saving/rc.local /etc/rc.local
+
+# Disable USB and Ethernet
+echo '1-1' |sudo tee /sys/bus/usb/drivers/usb/unbind
+
+sudo shutdown -r now
