82a581218d1eec85bce6fa33ae7356bfed730f6b
Joey-Michael Fallone <jm@sp0d3rm0b1le.local>
2018-03-19 13:30:12 -0600

    Power saving w/o reboot

--- pi_image/pi_scripts/power_saving/power_saver.sh
+++ pi_image/pi_scripts/power_saving/power_saver.sh
@@ -1,7 +1,17 @@
 # power saving tricks
 
 # requires reboot
-sudo mv /home/thor/capstone/pi_image/pi_scripts/power_saving/rc.local /etc/rc.local
+# sudo cp /home/thor/capstone/pi_image/pi_scripts/power_saving/rc.local /etc/rc.local
+
+# Disable HDMI
+/usr/bin/tvservice -o
+
+# Disable LEDs
+sudo sh -c 'echo none > /sys/class/leds/led0/trigger'
+sudo sh -c 'echo none > /sys/class/leds/led0/trigger'
+sudo sh -c 'echo 0 > /sys/class/leds/led0/brightness'
+sudo sh -c 'echo 0 > /sys/class/leds/led0/brightness'
+
 
 # Disable USB and Ethernet
 echo '1-1' |sudo tee /sys/bus/usb/drivers/usb/unbind
