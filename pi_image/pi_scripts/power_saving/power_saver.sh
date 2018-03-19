d07f66c9980156f446b93b683cc81ff0487186eb
thor <thor@capstone.fake>
2018-03-19 13:30:50 -0600

    power saving w/o reboot

--- pi_image/pi_scripts/power_saving/power_saver.sh
+++ pi_image/pi_scripts/power_saving/power_saver.sh
@@ -6,4 +6,4 @@ sudo mv /home/thor/capstone/pi_image/pi_scripts/power_saving/rc.local /etc/rc.lo
 # Disable USB and Ethernet
 echo '1-1' |sudo tee /sys/bus/usb/drivers/usb/unbind
 
-sudo shutdown -r now
+ sudo shutdown -r now
