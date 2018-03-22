8adf84d75f748d0056c9fbd46d40522d8daffea2
thor <thor@capstone.fake>
2018-03-22 15:02:16 -0600

    Various bug fixes

--- pi_image/sensors/interfacing/SDL/runStation.py
+++ pi_image/sensors/interfacing/SDL/runStation.py
@@ -17,6 +17,7 @@ if __name__ == '__main__':
   sudoPassword = str(base64.b64decode('czNuczBy'))
   # Creating the raw sensor data file that will go to the blob in Azure
   command = "sudo pkill -SIGUSR1 station"
+  sleep(10)
   command = custom_dir + '/Station ' + ' > ' + custom_dir + '/.data/station.dat'
   #p = os.system('echo %s|sudo -S %s' % (sudoPassword, command))
   os.popen("sudo -S %s"%(command), 'w').write(sudoPassword)
