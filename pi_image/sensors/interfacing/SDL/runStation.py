a44fbd78f1f1d1dd3f7008e595ee538e52a0094b
Joey-Michael Fallone <jm@sp0d3rm0b1le.local>
2018-03-22 15:04:39 -0600

    Bug Fixes

--- pi_image/sensors/interfacing/SDL/runStation.py
+++ pi_image/sensors/interfacing/SDL/runStation.py
@@ -7,6 +7,7 @@
 '''
 import os
 import base64
+import time
 
 custom_dir = "/home/thor/capstone/pi_image/sensors/interfacing/SDL"
 
@@ -18,7 +19,7 @@ if __name__ == '__main__':
   # Creating the raw sensor data file that will go to the blob in Azure
   command = "pkill -SIGUSR1 station"
   # wait for signal to trigger print
-  sleep(10)
+  time.sleep(10)
   command = custom_dir + '/Station ' + ' > ' + custom_dir + '/.data/station.dat'
   #p = os.system('echo %s|sudo -S %s' % (sudoPassword, command))
   os.popen("sudo -S %s"%(command), 'w').write(sudoPassword)
