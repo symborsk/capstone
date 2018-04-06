0a7900464a945a816e00baebd9e0be3fb930d8dd
symborski <symborsk@ualberta.ca>
2018-04-05 18:23:21 -0600

    Updated the console app. Added the .exe in the.

--- ai_model/AIServerConsoleApp/Program.cs
+++ ai_model/AIServerConsoleApp/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.IO;
+using Newtonsoft.Json.Linq;
 using System.Threading.Tasks;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
@@ -9,7 +11,7 @@ namespace AIServerConsoleApp
 {
     class Program
     {
-        static async void Main(string[] args)
+        static void Main(string[] args)
         {
             if (args.Length != 0)
             {
@@ -20,19 +22,28 @@ namespace AIServerConsoleApp
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=pcldevbgwilkinson01;AccountKey=NPkk2BjPvlG1Am78JrSi4ylEQNB3F6tacE/G8P3x8zLOe/BqZwvYMCXP+ni9KMwmx+px/f+J+n9QJq+v9eVSGg==;BlobEndpoint=https://pcldevbgwilkinson01.blob.core.windows.net/;QueueEndpoint=https://pcldevbgwilkinson01.queue.core.windows.net/;TableEndpoint=https://pcldevbgwilkinson01.table.core.windows.net/;FileEndpoint=https://pcldevbgwilkinson01.file.core.windows.net/");
             CloudBlobClient client = storageAccount.CreateCloudBlobClient();
 
-            List<CloudBlockBlob> rgBlobs = await GetAllList(client);
+            Task<List<CloudBlockBlob>> task = Task.Run(() => GetAllList(client));
+            List<CloudBlockBlob> rgBlobs  = task.Result;
 
             foreach(CloudBlockBlob blob in rgBlobs)
             {
-                String sDownloadedBlob = await blob.DownloadTextAsync();
+                Task<string> taskDownload = Task.Run(() => blob.DownloadTextAsync());
+                String sDownloadedBlob = taskDownload.Result;
+                String [] sDownloadedBlobs = sDownloadedBlob.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
 
+                foreach (string item in sDownloadedBlobs)
+                {
+                    JObject obj = JObject.Parse(item);
+                    string timestamp = obj["timestamp"].ToString();
+                    run_cmd(@"C:\git\ai_model\scripts\model.py", String.Format("-eval -json \'{0}\'", item));
+                    obj["Forecast"] = run_cmd(@"C:\git\ai_model\scripts\model.py", String.Format("-eval -json \'{0}\'", item));
+                }
             }
-
         }
 
-        static async void PushDataUpToAzure(CloudBlobClient cli, String pathToJson, string timestamp)
+        static async void PushDataUpToAzure(CloudBlobClient cli, string jsonObj, string timestamp)
         {
-            bool succ = await SendJsonToStorage(cli, pathToJson, timestamp);
+            bool succ = await SendJsonToStorage(cli, jsonObj, timestamp);
 
             if (succ)
                 Console.WriteLine("Success writing JSON file to azure storage");
@@ -42,7 +53,8 @@ namespace AIServerConsoleApp
 
         static async Task<bool> SendJsonToStorage(CloudBlobClient blobClient, string fullFilePath, string timeStamp)
         {
-           
+            BlobContinuationToken continuationToken = null;
+            BlobResultSegment resultSegment = null;
 
             try
             {
@@ -53,11 +65,19 @@ namespace AIServerConsoleApp
                 string pathToBlob = dtDateTime.Year + "//" + dtDateTime.Month + "//" + dtDateTime.Date;
 
                 // Retrieve reference to a previously created container
-                //TODO: query all the device names
                 CloudBlobContainer container = blobClient.GetContainerReference("sensor-hub");
-                CloudBlockBlob blob = container.GetBlockBlobReference(pathToBlob);
+                resultSegment = await container.ListBlobsSegmentedAsync(@"/logs/post" + pathToBlob, true, BlobListingDetails.All, 1000, continuationToken, null, null);
 
-                await blob.UploadFromFileAsync(fullFilePath);
+               if(resultSegment.Results.Count() ==  1)
+                {
+                    CloudAppendBlob blob =  (CloudAppendBlob) resultSegment.Results.First();
+                    await blob.AppendTextAsync(fullFilePath);
+                }
+                else
+                {
+                    CloudAppendBlob blob = container.GetAppendBlobReference(pathToBlob + "//" + Guid.NewGuid() + ".json");
+                    await blob.UploadTextAsync(fullFilePath);
+                }
             }
             catch(Exception e)
             {
@@ -84,6 +104,22 @@ namespace AIServerConsoleApp
             {
                 Console.WriteLine("Error getting latest blob: " + e.Message);
                 return null;
+            }
+        }
+
+        private static string run_cmd(string cmd, string args)
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = "my/full/path/to/python.exe";
+            start.Arguments = string.Format("{0} {1}", cmd, args);
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            using (Process process = Process.Start(start))
+            {
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    return reader.ReadToEnd();        
+                }
             }
         }
     }
