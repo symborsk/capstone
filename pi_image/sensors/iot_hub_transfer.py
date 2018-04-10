a2f1a197493362f8fbc18aa3f7b4fe8c75f714a2
Joey-Michael Fallone <jm@sp0d3rm0b1le.local>
2018-04-10 12:14:28 -0600

    python....

--- pi_image/sensors/iot_hub_transfer.py
+++ pi_image/sensors/iot_hub_transfer.py
@@ -47,7 +47,7 @@ lat, lon = 53.5273, -113.5295
 # TODO: Look into somehow providing this dynamically
 # original
 with open('/home/thor/.connection_string.dat') as data_file:
-	connectionString = data_file.readline().strip("\n")
+    connectionString = data_file.readline().strip("\n")
 
 print(connectionString)
  
@@ -127,18 +127,18 @@ class IoTHub:
 
 """ Function that loads JSON output files & returns a single JSON string for the hub
 
-	Inputs
-		path: Folder path where output JSON files are stored
-		files: List of JSON filenames to be loaded
-	Outputs
-		JSON string for the sensor hub message
+    Inputs
+        path: Folder path where output JSON files are stored
+        files: List of JSON filenames to be loaded
+    Outputs
+        JSON string for the sensor hub message
 """
 def get_output(path=output_path, output=output_file, count=sensor_count):
-	print("getting output")
-	with open("/home/thor/.interval.dat") as interval_file:
-		polling_frequency = interval_file.readline()
-		polling_frequency = float(polling_frequency)/60
-	# Load all of the JSON objects then truncate the file
+    print("getting output")
+    with open("/home/thor/.interval.dat") as interval_file:
+        polling_frequency = interval_file.readline()
+        polling_frequency = float(polling_frequency)/60
+    # Load all of the JSON objects then truncate the file
     with open("/home/thor/.email.dat", "r") as email_file:
         email = email_file.readline().strip("\n")
 
@@ -147,33 +147,33 @@ def get_output(path=output_path, output=output_file, count=sensor_count):
     else:
         use_3g = "false"
 
-	sensors = []
-	with open(path + output, 'r+') as f:
-		# The 0th element is blank since all sensor programs lead with newline
-		contents = f.read().split('\n')[1:]
-		f.truncate(0)
+    sensors = []
+    with open(path + output, 'r+') as f:
+        # The 0th element is blank since all sensor programs lead with newline
+        contents = f.read().split('\n')[1:]
+        f.truncate(0)
 
-	# Load all of the JSON objects from the file
-	for c in contents:
-		sensors += [json.loads(c)]
+    # Load all of the JSON objects from the file
+    for c in contents:
+        sensors += [json.loads(c)]
 
-	# Build the JSON object that will be transmitted
-	output = {
-				"device_name": hub_name,
-				"timestamp": timestamp,
+    # Build the JSON object that will be transmitted
+    output = {
+                "device_name": hub_name,
+                "timestamp": timestamp,
         "polling_frequency": polling_frequency,
         "battery_temp_ro": float(bat_temp),
         "email_address":  email,
         "use_3G": 
-				"location": {
-					"lat": lat,
-					"lon": lon
-				},
-				"sensors": sensors
-			}
+                "location": {
+                    "lat": lat,
+                    "lon": lon
+                },
+                "sensors": sensors
+            }
 
-	# Return the output in JSON string format
-	return json.dumps(output, default=lambda o: o.__dict__, 
+    # Return the output in JSON string format
+    return json.dumps(output, default=lambda o: o.__dict__, 
             sort_keys=True, indent=4)
 
 """ Function that takes received JSON data (for sensor hub settings) and 
@@ -188,10 +188,10 @@ def get_output(path=output_path, output=output_file, count=sensor_count):
     https://stackoverflow.com/questions/6578986/how-to-convert-json-data-into-a-python-object?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 """
 def parse_message(message):
-	message = message[0]
-	x = json.loads(message, 
+    message = message[0]
+    x = json.loads(message, 
             object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
-	return x
+    return x
 
 """ Function that takes received obj and parses out changes to settings
 
@@ -199,11 +199,11 @@ def parse_message(message):
         settings: The object containing new settings
 """
 def update_settings(settings):
-	print(dir(settings))
-	try:
-		seconds = int(settings.polling_frequency) * 60
-		with open("/home/thor/.interval.dat", "w+") as file:
-			file.write(str(seconds))
+    print(dir(settings))
+    try:
+        seconds = int(settings.polling_frequency) * 60
+        with open("/home/thor/.interval.dat", "w+") as file:
+            file.write(str(seconds))
         email = str(settings.email_address)
         with open("/home/thor/.email.dat") as file:
             file.write(email)
@@ -211,32 +211,32 @@ def update_settings(settings):
             f = open("/home/thor/.use3G.dat")
         else:
             os.system("sudo rm -rf /home/thor/.use3G.dat")
-	except:
-		print("no polling freq?")
+    except:
+        print("no polling freq?")
 
 if __name__=='__main__':
-	# Initialize the connection
-	IoTHubConn = IoTHub(connectionString)
-	print(connectionString)
+    # Initialize the connection
+    IoTHubConn = IoTHub(connectionString)
+    print(connectionString)
 
 
-	try:
-		# Receive any messages from the IoT Hub
-		response = IoTHubConn.receiveC2DMsg(deviceId)
-		print(str(response))
-		try:
-			responseObj =  parse_message(response)
-			#sanitize the etag 
-			etag = response[2]['ETag']
-			etag = etag.replace('"', '').strip()		
-			ackRespo = IoTHubConn.ackC2DMsg(deviceId, etag)
-			print(str(ackRespo))
-			update_settings(responseObj)
-		except:
-			pass
-		# Send data to the IoT Hub
-		body = get_output()
-		print(body)
-		response = IoTHubConn.sendD2CMsg(deviceId, body)
-	except OSError as err:
-		print('Error: ' + str(err))
+    try:
+        # Receive any messages from the IoT Hub
+        response = IoTHubConn.receiveC2DMsg(deviceId)
+        print(str(response))
+        try:
+            responseObj =  parse_message(response)
+            #sanitize the etag 
+            etag = response[2]['ETag']
+            etag = etag.replace('"', '').strip()        
+            ackRespo = IoTHubConn.ackC2DMsg(deviceId, etag)
+            print(str(ackRespo))
+            update_settings(responseObj)
+        except:
+            pass
+        # Send data to the IoT Hub
+        body = get_output()
+        print(body)
+        response = IoTHubConn.sendD2CMsg(deviceId, body)
+    except OSError as err:
+        print('Error: ' + str(err))
